066fd486e7cc07f4cb1969b161a93a5973157941
Vitor Barbosa <46977318+S00185772@users.noreply.github.com>
2020-02-25 17:59:23 +0000

    RayCastWeapons

---
+++ Assets/Scriptes/PlayerAttack.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAttack : MonoBehaviour {
+
+    public Weapon[] weapons;
+    int activeWeaponIndex = -1;
+    Weapon activeWeapon;
+	void Start () {
+
+        SetActveWeapon(0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        HandleWeaponSwitching();
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+
+
+            if (activeWeapon)
+            {
+                activeWeapon.Fire(transform.position);
+            }
+
+        }
+
+
+    }
+
+    private void HandleWeaponSwitching()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetActveWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetActveWeapon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetActveWeapon(2);
+        }
+    }
+
+    private void SetActveWeapon(int index)
+    {
+        if(index != activeWeaponIndex)
+        {
+            if(index >= 0 && index <= weapons.Length)
+            {
+                if (activeWeapon)
+                    Destroy(activeWeapon.gameObject);
+
+                activeWeapon = Instantiate(weapons[index], transform);
+                activeWeaponIndex = index;            }
+        }
+
+    }
+    
+}
---
+++ Assets/Scriptes/Shotgun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : RayCastWeapon {
+
+
+    public Vector3 Spread = new Vector3(0.1f, 0, 0);
+
+    public override void Fire(Vector3 fireFromPosition)
+    {
+        ShootRay(fireFromPosition, transform.forward);
+        ShootRay(fireFromPosition,transform.forward+Spread);
+        ShootRay(fireFromPosition, transform.forward-Spread);
+
+
+        base.Fire(fireFromPosition);
+    }
+
+    private void ShootRay(Vector3 position,Vector3 direction)
+    {
+        if (Physics.Raycast(position, direction, out raycastHit, Range))
+        {
+            HealthComponent health = raycastHit.collider.GetComponent<HealthComponent>();
+            if (health)
+            {
+                health.ApplyDamage(DamagePerHit);
+            }
+        }
+    }
+}
