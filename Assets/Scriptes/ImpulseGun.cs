f4153077d00d5f33b15285e41af078d2cdb4405c
Vitor Barbosa <46977318+S00185772@users.noreply.github.com>
2020-02-27 17:44:49 +0000

    Finished

---
+++ Assets/Scriptes/ImpulseGun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseGun : RayCastWeapon {
+
+    public float ImpulseAmount;
+
+    public override void Fire(Vector3 fireFromPosition)
+    {
+         if(Physics.Raycast(fireFromPosition,transform.forward,out raycastHit, Range))
+        {
+            Rigidbody health = raycastHit.collider.GetComponent<Rigidbody>();
+            if (health)
+            {
+                Vector3 direction = transform.position - health.transform.position;
+                health.AddForce(-direction * ImpulseAmount, ForceMode.Impulse);
+
+            }
+        }
+
+
+
+        base.Fire(fireFromPosition);
+    }
+
+}
